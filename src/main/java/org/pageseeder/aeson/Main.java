76accbd18faa56e85e6bb931a623401029fa9e58
Christophe Lauret <tyenki@gmail.com>
2015-08-01 11:29:36 +1000

    Switched to Maven, updated namespace and licence

--- src/main/java/org/pageseeder/aeson/Main.java
+++ src/main/java/org/pageseeder/aeson/Main.java
@@ -1,11 +1,19 @@
 /*
- * This file is part of the Aeson library.
+ * Copyright 2010-2015 Allette Systems (Australia)
+ * http://www.allette.com.au
  *
- * For licensing information please see the file license.txt included in the release.
- * A copy of this licence can also be found at
- *   http://www.opensource.org/licenses/artistic-license-2.0.php
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package org.weborganic.aeson;
+package org.pageseeder.aeson;
 
 import java.io.File;
 
